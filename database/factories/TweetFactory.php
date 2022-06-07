0dd20eac28f9830f35855031c63df12f1b7f5d20
Marc Mendoza <777marc@gmail.com>
2022-06-07 17:43:33 -0400

    adding tweets

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class TweetFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'body' => $this->faker->sentence(20),
+            'created_at' => $this->faker->dateTimeBetween('-30 days', now())
         ];
     }
 }
